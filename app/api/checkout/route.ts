b6b82dc46080075e48253562bd738b0c8d9cd866
Vittoriobre <vittoriobre@gmail.com>
2025-11-15 06:06:45 +0100

    fix stripe api version for build

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -3,7 +3,7 @@ import { supabase } from "@/lib/supabase";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2025-02-24.acacia",
 });
 
 export async function GET(request: NextRequest) {
